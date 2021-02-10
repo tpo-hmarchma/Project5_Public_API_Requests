6b28093450e2588b12da6bc89c501b3837980a7b
tpo-hmarchma <77006539+tpo-hmarchma@users.noreply.github.com>
2021-02-10 15:00:37 -0600

    Fetch and return to create gallery

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,31 @@
  * Request 12 random users from the API in a single request
  * Specified nationality in request in order to make formatting easier in later steps
  * New random employee information displays each time page refreshes
- * Directory displays 12 random users with image, first name, last name, email, and location
- */
+*/
+
 fetch('https://randomuser.me/api/?nat=us&results=12')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => generateGallery(data.results))
+  .catch(err => console.log('Error fetching employees:', err));
+
+/**
+ * Directory displays 12 random users with image, first name, last name, email, and location
+*/
+function generateGallery (data) {
+  for (i=0; i < data.length; i++) {
+    let employee = data[i];
+    const galleryCard = `
+    <div class="card">
+     <div class="card-img-container">
+     <img class="card-img" src=${employee.picture.thumbnail} alt="profile picture">
+    </div>`;
+    const galleryContainer = `
+    <div class="card-info-container">
+     <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+     <p class="card-text">${employee.email}</p>
+     <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+    </div>
+    </div>`;
+    document.querySelector('#gallery').insertAdjacentHTML('beforeend', galleryCard + galleryContainer);
+  }
+}
