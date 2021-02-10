1407b5099f7961cfdd6f58c400bd4841f2287f23
tpo-hmarchma <77006539+tpo-hmarchma@users.noreply.github.com>
2021-02-10 08:14:43 -0600

    Added single request to get 12 users

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
-var employeeRequest = new XMLHttpRequest();
-employeeRequest.onreadystatechange = function() {}
+/**
+ * Request 12 random users from the API in a single request
+ * Specified nationality in request in order to make formatting easier in later steps
+ * New random employee information displays each time page refreshes
+ * Directory displays 12 random users with image, first name, last name, email, and location
+ */
+fetch('https://randomuser.me/api/?nat=us&results=12')
+  .then(response => response.json())
+  .then(data => console.log(data));
