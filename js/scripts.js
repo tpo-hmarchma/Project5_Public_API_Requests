0b85540797879155655d30aac45b6043895fd491
tpo-hmarchma <77006539+tpo-hmarchma@users.noreply.github.com>
2021-02-12 08:04:09 -0600

    setModalContent function to update data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-    //TODO:
+   //TODO:
     //Remove modal buttons for exceeds if I don't go for exceeds
 
 /**
@@ -11,6 +11,8 @@
 
 // Variable for address of API with specific results requested
 const randomUsersUrl = 'https://randomuser.me/api/?nat=us&results=12';
+// Blank array to add results to
+let employeeArray = [];
 
 async function getRandomEmployees (url) {
   const getRandomEmployeesResponse = await fetch(url);
@@ -19,7 +21,10 @@ async function getRandomEmployees (url) {
 }
 
 getRandomEmployees(randomUsersUrl)
-  .then(data => generateDirectory(data.results))
+  .then(data => {
+    generateDirectory(data.results);
+    employeeArray = data.results;
+  })
   .then(() => generateModalCards())
   .catch(err => console.log('Error fetching employees:', err));
 
@@ -29,7 +34,7 @@ getRandomEmployees(randomUsersUrl)
 // generateDirectory maps over the returned employee data and creates directory cards for each employee
 function generateDirectory (data) {
   let directory = data.map((employee, index) =>
-    `<div class="card">
+    `<div class="card" id="${index}">
       <div class="card-img-container">
       <img class="card-img" src=${employee.picture.thumbnail} alt="profile picture">
      </div>
@@ -76,37 +81,35 @@ function createModal () {
   });
 }
 
-// generateModalCards targets all cards and adds an event listener to open modal windo when card is clicked
+// generateModalCards targets all cards and adds an event listener to open modal window when card is clicked
 function generateModalCards () {
   const directoryCards = document.querySelectorAll('.card');
+  let selectedCard = '';
+  // Adds click listener to each card to display the modal container
   directoryCards.forEach(card => {
     card.addEventListener('click', (e) => {
       document.querySelector('.modal-container').style.display = 'block';
-      // let person = e.currentTarget.index;
-      console.log(e.target.index);
-      // console.log(person);
+      selectedCard = e.currentTarget;
+      console.log(selectedCard.id);
+      setModalContent(selectedCard.id);
     });
   });
 }
 
-// let person = e.Target, and then get the card-text (which is the email), and use that to get the info?
-// maybe go back and put all info in an array?
-
-
 // function setModalData to take employee data and add it to blank modal window
-function setModalContent (employee, data, index) {
+function setModalContent (id) {
   const modalInfoContainer = document.querySelector('.modal-info-container');
   modalInfoContainer.innerHTML = '';
   let modalCardInfo = 
-    `
-    <img class="modal-img" src=${employee.picture.large} alt="profile picture">
-    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-    <p class="modal-text">${employee.email}</p>
-    <p class="modal-text cap">${employee.location.city}</p>
-    <hr>
-    <p class="modal-text">${employee.phone}</p>
-    <p class="modal-text">${employee.location.street} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-    <p class="modal-text">Birthday: ${employee.dob.date}</p>
+  `
+  <img class="modal-img" src=${employeeArray[id].picture.large} alt="profile picture">
+  <h3 id="name" class="modal-name cap">${employeeArray[id].name.first} ${employeeArray[id].name.last}</h3>
+  <p class="modal-text">${employeeArray[id].email}</p>
+  <p class="modal-text cap">${employeeArray[id].location.city}</p>
+  <hr>
+  <p class="modal-text">${employeeArray[id].phone}</p>
+  <p class="modal-text">${employeeArray[id].location.street} ${employeeArray[id].location.city}, ${employeeArray[id].location.state} ${employeeArray[id].location.postcode}</p>
+  <p class="modal-text">Birthday: ${employeeArray[id].dob.date}</p>
   `;
   modalInfoContainer.insertAdjacentHTML('afterbegin', modalCardInfo);
-}
+  }
